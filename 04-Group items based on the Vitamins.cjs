d213843ebffe9c19c9590fa335f706c816ebf884
Rajashekarmanda <rajashekarmanda3729@gmail.com>
2023-03-17 12:47:39 +0530

    writen diff logic

--- 04-Group items based on the Vitamins.cjs
+++ 04-Group items based on the Vitamins.cjs
@@ -1,23 +1,19 @@
 const items = require('./3-arrays-vitamins.cjs')
-let vitaminsArray = []
-let groupByVitamins = {}
+let vitaminsArray = [], groupByVitamins = {}
 items.map((eachVitamin) => {
-    eachVitamin.contains.split(',').map((eachProduct)=>{
-        if (!vitaminsArray.includes(eachProduct)){
-            vitaminsArray.push(eachProduct.trim())
-        }
-    })
+    eachVitamin.contains.split(',').map((eachProduct) => (!vitaminsArray.includes(eachProduct)) ? vitaminsArray.push(eachProduct.trim()) : '')
 })
-for (let eachVitamin of new Set(vitaminsArray)){         // used set dataStructure to remove duplicates...
+for (let eachVitamin of new Set(vitaminsArray)) {
     items.map((eachProduct) => {
-        if (eachProduct.contains.includes(eachVitamin) || eachProduct.contains.endsWith(eachVitamin)){
-            if (groupByVitamins[eachVitamin]===undefined){
-                groupByVitamins[eachVitamin]=[]
+        if (eachProduct.contains.includes(eachVitamin) || eachProduct.contains.endsWith(eachVitamin)) {
+            if (groupByVitamins[eachVitamin] === undefined) {
+                groupByVitamins[eachVitamin] = []
                 groupByVitamins[eachVitamin].push(eachProduct.name)
-            }else{
+            } else {
                 groupByVitamins[eachVitamin].push(eachProduct.name)
             }
         }
     })
 }
 console.log(groupByVitamins)
+
